3c20145a1c8343da6eb0015fd8bf318e05ad14ff
TahaBilalCS <bilaltahacs@gmail.com>
2021-07-18 15:00:14 -0400

    Setup IDE linting and prettier

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import * as process from "process";
+import * as process from 'process';
 
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
+	dotenv.config();
 }
+
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 
@@ -15,8 +16,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send(`PORTS ${PORT}, NODE:${process.env.NODE_ENV}`)
-  // res.send('<h1>Hello from the TypeScript world! </h1>');
+	res.send(`PORTS ${PORT}, NODE:${process.env.NODE_ENV}`);
+	// res.send('<h1>Hello from the TypeScript world! </h1>');
 });
 
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
