2c8ebc198086ecf908ba45d07684dbcfd661b3ff
TahaBilalCS <bilaltahacs@gmail.com>
2021-07-18 16:29:28 -0400

    Hi

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 3000;
 const app: Express = express();
 console.log('hi');
 console.log('hi');
-console.log('hi');
 
 app.use(helmet());
 app.use(bodyParser.json());
