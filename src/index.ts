964430cab57e0fb880f604671960b550c8395f89
TahaBilalCS <bilaltahacs@gmail.com>
2021-07-18 15:37:09 -0400

    Setup IDE linting and prettier

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
 console.log('hi');
+console.log('bue');
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
