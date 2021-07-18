0382c239584d7a7572579d1563fb75731d7cfa04
TahaBilalCS <bilaltahacs@gmail.com>
2021-07-18 03:24:16 -0400

    Get typescript to work on dev

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,26 @@
 module.exports = {
-  apps: [{
-    name: 'BackendBill',
-    script: './index.js'
-  }],
-  deploy: {
-    production: {
-      user: 'ubuntu',
-      host: 'ec2-3-231-208-118.compute-1.amazonaws.com',
-      key: '~/.ssh/billbo-key.pem',
-      ref: 'origin/main',
-      repo: 'git@github.com:TahaBilalCS/BackendBill.git',
-      path: '/home/ubuntu/BackendBill',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    apps: [{
+        name: 'BackendBill',
+        script: './dist/index.js',
+        // env: {
+        //     NODE_ENV: "development"
+        // },
+        // env_production: {
+        //     NODE_ENV: "production"
+        // },
+        // emv_test:{
+        //     NODE_ENV: "test"
+        // }
+    }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-3-231-208-118.compute-1.amazonaws.com',
+            key: '~/.ssh/billbo-key.pem',
+            ref: 'origin/main',
+            repo: 'git@github.com:TahaBilalCS/BackendBill.git',
+            path: '/home/ubuntu/BackendBill',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+        }
     }
-  }
 }
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import * as process from "process";
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`PORT455477 ${PORT}, NODE:${process.env.NODE_ENV}`)
+  // res.send('<h1>Hello from the TypeScript world! </h1>');
+});
+
+app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
