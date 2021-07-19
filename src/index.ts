e080c35890f60be4d29f1bbc1de9266c5e2a3d31
TahaBilalCS <bilaltahacs@gmail.com>
2021-07-19 02:34:37 -0400

    Fix deploy

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    res.send(`PORTSNEW ${PORT}, NODE:${PORT}`);
+    res.send(`PORTSNEW ${PORT}, NODE:${process.env.NODE_ENV}`);
     // res.send('<h1>Hello from the TypeScript world! </h1>');
 });
 
