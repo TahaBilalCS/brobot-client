2fcf39a150c738e03869a27ec22b080418b3052f
TahaBilalCS <bilaltahacs@gmail.com>
2021-07-21 21:01:51 -0400

    Fix tslin prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from "express";
-import bodyParser from "body-parser";
-import helmet from "helmet";
-import dotenv from "dotenv";
-import * as process from "process";
+import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import * as process from 'process';
 
 export const sum = (...a: number[]): number => a.reduce((acc, val) => acc + val, 0);
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
     dotenv.config();
 }
 
@@ -17,7 +17,7 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     res.send(`PORTSNEW ${PORT}, NODE:${process.env.NODE_ENV}`);
     // res.send('<h1>Hello from the TypeScript world! </h1>');
