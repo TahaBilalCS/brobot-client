2a56a4f48d76840fb75b9b9a0b4d55b14ef538e6
TahaBilalCS <bilaltahacs@gmail.com>
2021-09-12 21:41:32 -0400

    Initial Project Setup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 module.exports = {
     apps: [
         {
-            name: "BackendBill",
-            script: "dist/index.js",
+            name: 'BackendBill',
+            script: 'dist/index.js',
             env: {
-                NODE_ENV: "development"
+                NODE_ENV: 'development'
             },
             env_production: {
-                NODE_ENV: "production"
+                NODE_ENV: 'production'
             }
             // emv_test:{
             //     NODE_ENV: "test"
@@ -16,13 +20,17 @@ module.exports = {
     ],
     deploy: {
         production: {
-            user: "ubuntu",
-            host: "ec2-3-231-208-118.compute-1.amazonaws.com",
-            key: "~/.ssh/billbo-key.pem",
-            ref: "origin/main",
-            repo: "git@github.com:TahaBilalCS/BackendBill.git",
-            path: "/home/ubuntu/BackendBill",
-            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env production"
+            user: 'ubuntu',
+            host: 'ec2-3-231-208-118.compute-1.amazonaws.com',
+            key: '~/.ssh/billbo-key.pem',
+            ref: 'origin/main',
+            repo: 'git@github.com:TahaBilalCS/BackendBill.git',
+            path: '/home/ubuntu/BackendBill',
+            env: {
+                TWITCH_CLIENT_ID: process.env.TWITCH_CID,
+                TWITCH_SECRET: process.env.TWITCH_CID
+            },
+            'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env production'
         }
     }
 };
