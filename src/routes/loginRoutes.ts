909d597916cb5ecbd373de3dfe0eea9b282a1239
TahaBilalCS <bilaltahacs@gmail.com>
2021-09-14 20:29:20 -0400

    Testing custom auth middleware

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import process from 'process';
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 // Because of body parser's lack of type, Interface now has all the same properties as Request. Overrides Request.body
 interface RequestWithBody extends Request {
@@ -8,16 +8,38 @@ interface RequestWithBody extends Request {
     body: { [key: string]: string | undefined };
 }
 
+/**
+ * Middleware for requiring authentication
+ * @param req
+ * @param res
+ * @param next
+ */
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+    if (req.session.loggedIn) {
+        next();
+        return;
+    }
+
+    res.status(403);
+    res.send('Not permitted');
+}
+
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
     // res.send('<h1>Hello from the TypeScript world! </h1>');
-    if (req.session /*&& req.session.passport && req.session.passport.user*/) {
-        // Authenticated
-        res.send(`<h1>SIGNED IN</h1>- PORT: -, NODE:${process.env.NODE_ENV}`);
+    if (req.session.loggedIn /*&& req.session.passport && req.session.passport.user*/) {
+        // User authenticated
+        res.send(`
+            <h1>SIGNED IN</h1> - PORT: -, NODE:${process.env.NODE_ENV}
+            <a href='/logout'>Logout</a>
+        `);
     } else {
         // res.send('<html><head><title>Twitch Auth Sample</title></head><a href="/auth/twitch"><img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png"></a></html>');
-        res.send(`PORT: -, NODE:${process.env.NODE_ENV}`);
+        res.send(`
+        PORT: -, NODE:${process.env.NODE_ENV}
+        <a href='/login'>Login</a>
+        `);
     }
 });
 
@@ -47,5 +69,17 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
     }
 });
 
+router.get('/logout', (req: Request, res: Response) => {
+    req.session.loggedIn = undefined;
+    res.redirect('/');
+});
+
+// Apply middleware first
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send(`
+            <h1>SIGNED IN</h1> 
+            <a href='/logout'>Logout</a>
+        `);
+});
 // Curly brace when declaration (const router = Router) and export are not on the same line. It's an already created var
 export { router };
