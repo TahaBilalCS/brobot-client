b34640920f671d23f83bd2d106427fdbc4c2d7d1
TahaBilalCS <bilaltahacs@gmail.com>
2021-10-06 23:24:11 -0400

    Test deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -21,7 +21,7 @@ module.exports = {
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-3-231-208-118.compute-1.amazonaws.com',
+            host: 'ec2-34-234-175-84.compute-1.amazonaws.com',
             key: '~/.ssh/billbo-key.pem',
             ref: 'origin/main',
             repo: 'git@github.com:TahaBilalCS/BackendBill.git',
