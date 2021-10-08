33fc72dd3ea3659fdc9239794266232abb5f1599
TahaBilalCS <bilaltahacs@gmail.com>
2021-10-08 19:51:15 -0400

    Test deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     apps: [
         {
-            name: 'BackendBill',
+            name: 'brobot',
             script: 'dist/index.js',
             env: {
                 NODE_ENV: 'development',
