228c2896a93e1427870433981c55d08c1491cfbd
TahaBilalCS <bilaltahacs@gmail.com>
2021-10-08 19:47:43 -0400

    Test deploy

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,21 +1,29 @@
-const commonEnv = require('./config/commonEnv.cjs')
+require('dotenv').config();
 
 module.exports = {
     apps: [
         {
-            name: 'brobot',
+            name: 'BackendBill',
             script: 'dist/index.js',
-            env_development: {
-                ...commonEnv.dev,
+            env: {
+                NODE_ENV: 'development',
+                TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+                TWITCH_SECRET: process.env.TWITCH_SECRET,
+                TEST_SECRET: process.env.TEST_SECRET,
+                TWITCH_CALLBACK_URL: process.env.TWITCH_CALLBACK_URL,
+                SESSION_SECRET: process.env.SESSION_SECRET,
             },
             env_production: {
-                ...commonEnv.prod
+                NODE_ENV: 'production',
+                TWITCH_CLIENT_ID: process.env.PROD_TWITCH_CLIENT_ID,
+                TWITCH_SECRET: process.env.PROD_TWITCH_SECRET,
+                TEST_SECRET: process.env.PROD_TEST_SECRET,
+                TWITCH_CALLBACK_URL: process.env.PROD_TWITCH_CALLBACK_URL,
+                SESSION_SECRET: process.env.PROD_SESSION_SECRET,
             },
-            env_test:{
+            env_test: {
 
-            },
-            watch_delay: 1000,
-            ignore_watch: ['node_modules']
+            }
         }
     ],
     deploy: {
@@ -24,11 +32,9 @@ module.exports = {
             host: 'ec2-34-234-175-84.compute-1.amazonaws.com',
             key: '~/.ssh/billbo-key.pem',
             ref: 'origin/main',
-            repo: 'git@github.com:TahaBilalCS/brobot.git',
-            path: '/home/ubuntu/brobot',
-            env: {
-                ...commonEnv.prod
-            },
+            repo: 'git@github.com:TahaBilalCS/BackendBill.git',
+            path: '/home/ubuntu/BackendBill',
+            // env: {}
             'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env test --update-env'
         }
     }
