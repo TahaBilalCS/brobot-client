ef528552e393d1880fd7e099e71b32410034154b
TahaBilalCS <bilaltahacs@gmail.com>
2021-10-10 14:38:20 -0400

    Initial project setup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -24,17 +24,17 @@ module.exports = {
             // pm2 stop all to reload new environment variables
             env_production: {
                 NODE_ENV: 'production',
-                PORT: process.env.PORT,
-                DOMAIN: process.env.DOMAIN,
-                TEST_SECRET: process.env.PROD_TEST_SECRET,
-                SESSION_SECRET: process.env.PROD_SESSION_SECRET,
-                // We can't access some prod variables from localhost
-                TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
-                TWITCH_SECRET: process.env.TWITCH_SECRET,
-                TWITCH_CALLBACK_URL: process.env.TWITCH_CALLBACK_URL,
-                //
-                MONGO_URI: process.env.PROD_MONGO_URI,
-                COOKIE_KEY: process.env.PROD_COOKIE_KEY
+                // PORT: process.env.PORT,
+                // DOMAIN: process.env.DOMAIN,
+                // TEST_SECRET: process.env.PROD_TEST_SECRET,
+                // SESSION_SECRET: process.env.PROD_SESSION_SECRET,
+                // // We can't access some prod variables from localhost
+                // TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+                // TWITCH_SECRET: process.env.TWITCH_SECRET,
+                // TWITCH_CALLBACK_URL: process.env.TWITCH_CALLBACK_URL,
+                // //
+                // MONGO_URI: process.env.PROD_MONGO_URI,
+                // COOKIE_KEY: process.env.PROD_COOKIE_KEY
             },
             // Don't overwrite injected variables on remote machine
             env_aws: {
@@ -67,7 +67,7 @@ module.exports = {
             // pm2 caches environment variables and --update-env doesn't seem to be working
             // Need to delete pm2 instance before starting instance again to update
             'post-deploy':
-                'npm install && npm run build && pm2 startOrRestart ecosystem.config.cjs --env aws --update-env'
+                'npm install && npm run build && pm2 restart/reload ecosystem.config.cjs --update-env [--env production]'
         }
     }
 };
