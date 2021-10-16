2a30248969d1bb4f211d06823f334a67d3bf9888
TahaBilalCS <bilaltahacs@gmail.com>
2021-10-16 03:21:35 -0400

    Setup Windows dev environment with python shell

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,30 @@
 import WebSocket from 'ws';
 import process from 'process';
 import { PythonShell } from 'python-shell';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// function startPythonShell() {
-//     // TODO
-//     let options = {
-//         pythonPath: 'C:\\Users\\Dynorama\\AppData\\Local\\Programs\\Python\\Python39\\python',
-//         scriptPath: '/home/brotherbill/brobot-client/src/py_commands'
-//     };
-//     PythonShell.run('disable_key.py', options, (err: any, results: any) => {
-//         if (err) {
-//             console.log('Err', err);
-//         } else {
-//             console.log('Success', results);
-//         }
-//     });
-// }
-//
-// startPythonShell();
+const scriptPath = path.join(__dirname, '../src/py_commands/');
+console.log(scriptPath)
+
+function startPythonShell() {
+    // TODO
+    let options = {
+        pythonPath: 'C:\\Users\\Bilal\\AppData\\Local\\Programs\\Python\\Python39\\python',
+        scriptPath: scriptPath
+    };
+    PythonShell.run('disable_key.py', options, (err: any, results: any) => {
+        if (err) {
+            console.log('Err', err);
+        } else {
+            console.log('Success', results);
+        }
+    });
+}
+
+startPythonShell();
 
 // console.log(process.env.WS_URL);
 //
