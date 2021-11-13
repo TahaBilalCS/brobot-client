92c29a440e8748efec417993dcf0e3103e268c45
TahaBilalCS <bilaltahacs@gmail.com>
2021-11-12 23:03:26 -0500

    hotkey

--- src/py_commands/voiceban.py
+++ src/py_commands/voiceban.py
@@ -1,10 +1,10 @@
 import time
 import sys
 from ahk import AHK
-ahk = AHK()
+ahk = AHK(executable_path="C:\Program Files\AutoHotkey\AutoHotkey.exe")
 time_to_sleep=int(sys.argv[1]) # seconds
 
 ahk.key_press('F8')
 time.sleep(time_to_sleep)
-ahk.key_press('F8')
+ahk.key_press('F9')
 quit()
